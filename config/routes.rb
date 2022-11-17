b7e77184504be47d6e59acedcdc1b5077d121c9b
ManamiKurimoto <114746043+kurimoto-manami@users.noreply.github.com>
2022-11-17 13:01:47 +0000

    Initial Commit4

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/homes/about' => 'homes#about',as:'about'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:show, :edit]
-  # get 'books/edit'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
