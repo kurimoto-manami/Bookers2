7cb420cccbc58da72a2dcccd8b493a4ff8a68ece
ManamiKurimoto <114746043+kurimoto-manami@users.noreply.github.com>
2022-11-18 13:02:18 +0000

    Initial Commit5

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, length: { maximum: 200 }, presence: true
+
   # def get_image
   #   unless image.attached?
   #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
