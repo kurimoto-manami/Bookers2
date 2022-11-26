3963c16f94d1feafd380fe5b2c4f4a80a87586f9
ManamiKurimoto <114746043+kurimoto-manami@users.noreply.github.com>
2022-11-26 12:37:07 +0000

    Initial Commit11

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # validates :name, presence: true
-  # validates :email, uniqueness: true
-  # validates :password, presence: true
 end
